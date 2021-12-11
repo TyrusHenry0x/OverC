666998291fa2a736d8405496880dac813f381073
tyrus <tyrushenry03@gmail.com>
2021-12-11 10:51:11 -0500

    header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,8 +5,20 @@ const Header = () => {
   return (
     <nav>
       <div className="nav">
-
+        <NavLink to="/">
+          <img className="header-logo" src='client/src/assets/images/logo.png' alt="logo" />
+        </NavLink>
+        <div className="header-btns">
+          <NavLink to="/constellations">
+            Constellation
+          </NavLink>
+          <NavLink to="/about">
+            About
+          </NavLink>
+        </div>
       </div>
     </nav>
   )
 }
+
+export default Header;
