efaaddd3d8e23a2c3502ec325ce4abcb292bea60
tyrus <tyrushenry03@gmail.com>
2021-12-11 12:46:24 -0500

    layout done

---
+++ client/src/components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import './Layout.css'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+
+const Layout = (props) => (
+  <div className="layout">
+    <Header />
+    <div className="layout-children">
+      {props.children}
+    </div>
+    <Footer />
+  </div>
+)
+
+export default Layout
