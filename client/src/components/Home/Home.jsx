a618b88c1e49ac7f4b440131e3ed2d8d9536aede
tyrus <tyrushenry03@gmail.com>
2021-12-13 18:30:23 -0500

    homescreen css

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -6,6 +6,9 @@ const Home = () => {
   return (
     <Layout>
       <div className="home-background-image">
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Maven+Pro&family=Pacifico&family=Roboto:wght@300&display=swap" rel="stylesheet" />
         <div className="home-content">
           <div className="welcome-text">
             <h1>Oversee your Future</h1>
