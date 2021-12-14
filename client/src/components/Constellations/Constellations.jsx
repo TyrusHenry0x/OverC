b90964f7b18464d0a51238e18f0609436bf1c959
tyrus <tyrushenry03@gmail.com>
2021-12-14 16:37:57 -0500

    styling components

--- client/src/components/Constellations/Constellations.jsx
+++ client/src/components/Constellations/Constellations.jsx
@@ -3,13 +3,15 @@ import './Constellations.css'
 const Constellations = (constellations) => {
   return (
     <div className="background-image">
-      <div className="Constellations">
+      <div className="constellations-box">
         <h1>Constellations</h1>
         {constellations.constellations.map((constellation) => (
-          <NavLink to={`/constellations/${constellation.id}`}>
-            <p key={constellation.id}>{constellation.name}
-            </p>
-          </NavLink>
+          <div className="link-box">
+            <NavLink className='link' to={`/constellations/${constellation.id}`}>
+              <p key={constellation.id}>{constellation.name}
+              </p>
+            </NavLink>
+          </div>
         ))}
       </div>
     </div >
