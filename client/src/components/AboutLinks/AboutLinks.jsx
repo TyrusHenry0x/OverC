b8645e5ec3eb0052b18850a5cd7eff07e90e7dc7
tyrus <tyrushenry03@gmail.com>
2021-12-14 17:43:15 -0500

    icons

--- client/src/components/AboutLinks/AboutLinks.jsx
+++ client/src/components/AboutLinks/AboutLinks.jsx
@@ -4,11 +4,11 @@ const AboutLinks = () => {
   return (
     <div className="about-links">
       <a href="https://github.com/TyrusHenry0x/OverC/blob/main/README.md">
-        <img src={require('../../assets/images/linkedin.svg').default} />
+        <img src={require("../../assets/icons/github.svg").default} />
       </a>
 
       <a href="https://www.linkedin.com/in/tyrus-henry-bb409021a/">
-        <img src={require('../../assets/github.svg').default} />
+        <img src={require('../../assets/icons/linkedin.svg').default} />
       </a>
     </div>
   )
